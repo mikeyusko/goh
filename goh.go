af2d471e1b88f4a7c2e5a81a6b3785bbbe83009f
Mike Yusko <mykhaylo.yusko@gmail.com>
2019-09-28 15:41:24 +0300

    deleted pointers

--- goh.go
+++ goh.go
@@ -4,10 +4,10 @@ import "reflect"
 
 // IncludeStrings replace a value with new value
 // and returns a new slice without modifying original.
-func Replace(values *[]int, old, new int) []int {
-	result := make([]int, len(*values))
+func Replace(values []int, old, new int) []int {
+	result := make([]int, len(values))
 
-	for i, v := range *values {
+	for i, v := range values {
 		if v == old {
 			result[i] = new
 		} else {
@@ -19,10 +19,10 @@ func Replace(values *[]int, old, new int) []int {
 
 // FilterStrings gets a slice of strings
 // and returns a new slice with values which passed condition check.
-func FilterStrings(values *[]string, condition func(v string) bool) []string {
+func FilterStrings(values []string, condition func(v string) bool) []string {
 	var result []string
 
-	for _, v := range *values {
+	for _, v := range values {
 		if condition(v) {
 			result = append(result, v)
 		}
@@ -32,10 +32,10 @@ func FilterStrings(values *[]string, condition func(v string) bool) []string {
 
 // FilterStrings gets a slice of integers
 // and returns a new one with values which passed condition check.
-func FilterInt(values *[]int, condition func(v int) bool) []int {
+func FilterInt(values []int, condition func(v int) bool) []int {
 	var result []int
 
-	for _, v := range *values {
+	for _, v := range values {
 		if condition(v) {
 			result = append(result, v)
 		}
@@ -75,10 +75,10 @@ func Between(values []int, value, beforeValue, afterValue int) bool {
 
 // MapString creates a new value based on function argument
 // and returns a new slice.
-func MapString(values *[]string, f func(v string) string) []string {
-	result := make([]string, len(*values))
+func MapString(values []string, f func(v string) string) []string {
+	result := make([]string, len(values))
 
-	for i, v := range *values {
+	for i, v := range values {
 		result[i] = f(v)
 	}
 	return result
@@ -86,10 +86,10 @@ func MapString(values *[]string, f func(v string) string) []string {
 
 // MapString creates a new value based on function argument
 // and returns a new slice.
-func MapInt(values *[]int, f func(v int) int) []int {
-	result := make([]int, len(*values))
+func MapInt(values []int, f func(v int) int) []int {
+	result := make([]int, len(values))
 
-	for i, v := range *values {
+	for i, v := range values {
 		result[i] = f(v)
 	}
 	return result
